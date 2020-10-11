763b70a5866b679dcd8c8b11d2d423c35b092847
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-10-11 13:14:11 +0200

    Minor patch

--- extensions/cparse_extensions.h
+++ extensions/cparse_extensions.h
@@ -1,12 +1,12 @@
 #ifndef CPARSE_EXTENSIONS_H
-#define CPARSE_EXTENSIONS_Hb
+#define CPARSE_EXTENSIONS_H
 
 packToken negation(const packToken& left, const packToken& right,
                  evaluationData* data) {
     return !right.asBool();
 }
 
-std::string tokTypeToString(const uint8_t& type) {
+std::string tokenTypeToString(const uint8_t& type) {
     switch (type) {
         case NONE: return "NONE";
         case OP: return "OP";
@@ -25,7 +25,7 @@ std::string tokTypeToString(const uint8_t& type) {
         case MAP: return "MAP";
         case REF: return "REF";
         case ANY_TYPE: return "ANY_TYPE";
-        default: return "UNKNOWN - PLEASE EXTEND THE 'tokTypeToString' extension";
+        default: return "UNKNOWN - PLEASE EXTEND THE 'tokenTypeToString' EXTENSION";
     }
 }
 
